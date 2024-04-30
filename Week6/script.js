578eee111f3ba547cf352742298cfa07778fd4a0
RuijianSun <sunruijian520@gmail.com>
2024-04-30 16:06:08 +1000

    6

---
+++ Week6/script.js
@@ -0,0 +1,27 @@
+const myHeading = document.querySelector("h1");
+console.log(myHeading);
+// console.log(myHeading,textContent)
+// myHeading.textContent = "new paragraph "
+// myHeading.style.backgroundColor = "limegreen"
+for (let i =0; 1<3; 1++)
+{
+    myHeading[i],textContent = "new" + i
+    myHeading[i].style.backgroundColour = "limegreen"
+}
+
+myHeading.forEach(changeMe)
+
+fouction changeMe(item)
+{
+    item,clasList.and("puper-box");
+}
+const myimage = document.querySelector("#my-image");
+console.log(myimage);
+myIiage,classsList,add("round");
+
+const myBotton =document.querySelector("#my-button");
+console.log( myBotton);
+
+myBotton.addEventListener("click", toggleMe);
+
+console.log(myImage.dataset,catname);
